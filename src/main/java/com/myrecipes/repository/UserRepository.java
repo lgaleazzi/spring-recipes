8a785a294177e660d17c75182db38393c170f282
Livia Galeazzi <liviagal@gmail.com>
2018-03-26 19:00:20 +0200

    Add do not export

--- src/main/java/com/myrecipes/repository/UserRepository.java
+++ src/main/java/com/myrecipes/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.myrecipes.repository;
 
 import com.myrecipes.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long>
 {
     User findByUsername(String username);
